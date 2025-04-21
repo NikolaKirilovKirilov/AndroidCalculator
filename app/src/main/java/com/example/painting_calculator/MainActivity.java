fbed7462ff46422fc1e4fbcc4c4e539dc7154ad1
Nikola Kirilov <nikolakirilov145@gmail.com>
2025-04-21 17:23:53 +0300

    Fixed some Null Pointer References

--- app/src/main/java/com/example/painting_calculator/MainActivity.java
+++ app/src/main/java/com/example/painting_calculator/MainActivity.java
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
-
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
